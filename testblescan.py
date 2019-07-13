f5757f5839f67f07c556ab7bb8c5250b3a3c8efe
RafaelKuebler <rafael_kuebler@yahoo.es>
2019-07-13 04:46:01 +0200

    Add post to bluetooth

--- testblescan.py
+++ testblescan.py
@@ -3,8 +3,18 @@
 
 import blescan
 import sys
+import requests
+import time
+import json
 
 import bluetooth._bluetooth as bluez
+
+API_ENDPOINT = "https://c2xolt5232.execute-api.eu-central-1.amazonaws.com/activeuser"
+
+ids = {
+    "fda50693a4e24fb1afcfc6eb07647825": "Alan",
+    "5da1ef28d3af4a87b66e568cc1e2ec9d": "Arne"
+}
 
 dev_id = 0
 try:
@@ -20,7 +30,14 @@ blescan.hci_enable_le_scan(sock)
 
 while True:
 	returnedList = blescan.parse_events(sock, 10)
-	print "----------"
 	for beacon in returnedList:
-		print beacon
+        split = returnedList.split(",")
+        beacon_id = split[1]
+        if beacon_id in ids and abs(int(split[-1])) < 40:
+            active_user = ids[beacon_id]
+            data = {"username": active_user}
+            
+            print("Sending")
+            response = requests.post(url = API_ENDPOINT, data = data)
+            print(response)
 
